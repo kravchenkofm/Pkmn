82ad188124bde47c29252e105e512e9b388efd36
kravchenkofm <wtrnkk88@gmail.com>
2024-10-17 17:28:19 +0300

    Update PokemonApplication.java

--- Pkmn/src/main/java/ru/mirea/pkmn/kravchenkofm/PokemonApplication.java
+++ Pkmn/src/main/java/ru/mirea/pkmn/kravchenkofm/PokemonApplication.java
@@ -3,7 +3,7 @@ package ru.mirea.pkmn.kravchenkofm;
 import ru.mirea.pkmn.Card;
 
 public class PokemonApplication {
-    static String filename = "C:\\Users\\user\\IdeaProjects\\Pkmn\\src\\main\\resources\\my_card.txt";
+    static String filename = "src\\main\\resources\\my_card.txt";
     public static void main(String[] args) throws Exception {
         CardImport cardimport = new CardImport();
         Card card = new Card();
@@ -11,7 +11,7 @@ public class PokemonApplication {
         CardExport.Export(card);
         System.out.println(card.toString());
 
-        Card Z = CardImport.SImport("C:\\Users\\user\\IdeaProjects\\Pkmn\\src\\main\\resources\\Blissey.crd");
+        Card Z = CardImport.SImport("src\\main\\resources\\Blissey.crd");
         System.out.printf(Z.toString());
     }
 }
